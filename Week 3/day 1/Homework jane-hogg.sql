a54f6203f8eef1613be6f79db948ff5ef8bce674
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-17 15:22:02 +0100

    Sql omni homework
    first draft of this

---
+++ Week 3/day 1/Homework jane-hogg.sql
@@ -0,0 +1,15 @@
+--MVP*
+--Q1
+--Find all the employees who work in the ‘Human Resources’ department.
+
+SELECT *
+FROM employees
+WHERE department = 'Human Resources'
+
+--Q2gett the first_name, last_name, and country of the employees who work in the ‘Legal’ department.
+
+SELECT *
+FROM employees
+WHERE department  = 'Legal'; 
+
+
