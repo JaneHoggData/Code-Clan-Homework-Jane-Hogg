ed999bbd01a1058bd5a3a7658ca44d91861e95ff
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-17 16:32:29 +0100

    Sql homework
    completed up to Q5 in CC

--- Week 3/day 1/Homework jane-hogg.sql
+++ Week 3/day 1/Homework jane-hogg.sql
@@ -6,10 +6,39 @@ SELECT *
 FROM employees
 WHERE department = 'Human Resources'
 
---Q2gett the first_name, last_name, and country of the employees who work in the ‘Legal’ department.
+--Q2get the first_name, last_name, and country of the employees who work in the ‘Legal’ department.
 
 SELECT *
 FROM employees
 WHERE department  = 'Legal'; 
+
+
+--Q3Count the number of employees based in Portugal.
+
+SELECT *
+FROM employees
+WHERE country = 'Portugal' 
+
+
+--Q4 Count the number of employees based in either Portugal or Spain. Portugal was 29. Spain 6
+
+
+SELECT 
+count(*) 
+FROM employees
+WHERE country = 'Portugal';
+
+
+SELECT 
+count(*)
+FROM employees e 
+WHERE country = 'Spain';
+
+
+--Q5 Count the number of pay_details records lacking a local_account_no.
+
+SELECT *
+FROM employees
+WHERE pay_detail_id = NULL;
 
 
