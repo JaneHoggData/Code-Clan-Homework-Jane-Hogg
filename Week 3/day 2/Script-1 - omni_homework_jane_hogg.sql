af4079d70c99b78d7e0285a9b4fb5d9f32c06e85
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-19 10:18:29 +0100

    Update Script-1 - omni_homework_jane_hogg.sql
    amended homework with the class pre pf the answers

--- Week 3/day 2/Script-1 - omni_homework_jane_hogg.sql
+++ Week 3/day 2/Script-1 - omni_homework_jane_hogg.sql
@@ -40,8 +40,89 @@ SELECT
     t.*
 FROM employees AS e
 INNER JOIN teams AS t
-ON t.id = e.team_id
-WHERE e.charge_cost  =
+ON e.team_id = t.id 
+WHERE CAST (t.charge_cost AS INT) > 80
+
+--Q2 Get a table of all employees details, together with their local_account_no and local_sort_code, if they have them.
+employees, LOCAL account AND sort code
+
+
+SELECT * FROM  employees;
+SELECT * FROM pay_details;
+
+
+SELECT 
+    e.*,
+    pd*,
+FROM  employees AS e;
+RIGHT JOIN pay_details AS pd 
+
+
+
+FROM pay_details AS pd;
+ON e.id = pd.id
+
+
+FROM pay_details pd 
+
+ON
+
+
+WHERE 
+
+
+
+
+--Q4 Create a table with the team id, team name and the count of the number of employees in each team.
+
+--team id, team NAMESPACE number
+--teams table and employees
+
+
+SELECT 
+  t.id,  
+  t.name,
+  COUNT(e.id)
+FROM employees AS e
+INNER JOIN teams AS t
+ON e.team_id = t.id
+GROUP BY t.id
+
+Q5
+
+
+SELECT  
+  t.name,
+  COUNT(e.id) * CAST(t.charge_cost AS INT) AS total_day_charge
+FROM employees AS e
+INNER JOIN teams AS t
+ON e.team_id = t.id
+GROUP BY t.id
+HAVING COUNT(e.id) * CAST(t.charge_cost AS INT) > 5000
+
+
+SELECT * FROM employees_committees;
+
+
+--Q5 - DISTINCT employees who ARE ON committees.id 
+SELECT count(DISTINCT (employee_id))
+FROM employees_committees
+
+
+--Q6 employees who are not on a committee.
+--show instances when not on a committee. And the count how man y peole there are in the committess.
+--filter for someof the data that is not required == anti join
+
+
+SELECT
+count(e.id )
+FROM  employees AS e
+LEFT JOIN employees_committees AS ec
+ON e.id = ec.employee_id 
+WHERE ec.committee_id IS NULL 
+
+
+-- from the last question we were aware of the 22 on the committee so could have subtracted from the 1000 emplyees that we have.
 
 
 
