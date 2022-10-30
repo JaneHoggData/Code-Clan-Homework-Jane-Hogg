8a43605f12cb272bd9d3f2e54969bb55423eda9b
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-10-30 19:35:09 +0000

    Disaster Date
    Not completed all Q3 yet - review the sum number for China total affexcted

--- Week 4 openended_cleaning/disaster_with_errorsjane_hogg.R
+++ Week 4 openended_cleaning/disaster_with_errorsjane_hogg.R
@@ -8,10 +8,14 @@ View(disasters_with_errors)
 head(disasters_with_errors)
 col(disasters_with_errors)
 
-#clean names with janitor
+#General review of the data
+
+
 
 disasters_with_errors%>%
   clean_names()
+
+#clean names with janitor
 
 disasters_with_errors%>%
 cols(iso = col_double())
@@ -31,7 +35,8 @@ Total_deathsnavalues <-disasters_with_errors%>%
   summarise(count = sum(is.na(total_deaths)))
 
 #Dropped all the NA values on the total deaths - previous check of NA
-#reduced the number of records to 460.
+#reduced the number of records to 460. This is a significant reduction from
+##12397 so need to understand this a little more.
 
 TotalDeathsclean <-disasters_with_errors%>%
   drop_na()
@@ -39,33 +44,65 @@ TotalDeathsclean <-disasters_with_errors%>%
 Total_deathclean
 
 #simple view of this data does give me the answer to the question 1
+##Needs more wokr 
 
 view(TotalDeathsclean)
 
 #Further code required to list only the top ten deaths - provides the result
-## Earthquake in Indonesia in 2004 resulting in 165,816 deaths
+## Earthquake in Indonesia in 2004 resulting in 165,816 deaths.
+##This is not correct.
+##Taking out the na also excludes the total damage variable and this
+##significantly reduces the data set.
 ## But review of the clean data shows that there are further disasters that
-##with higher death toll.  Return to this later in the day.
+##with higher death toll.  
 Finaltop_tenDeaths <-TotalDeathsclean%>%
   arrange(desc(total_deaths))%>%
   head(10)
           
   
+  Toptendeaths_no_na <- disasters_with_errors %>%
+    arrange (desc (total_deaths))%>%
+  head (10)
+
+  #This provides the full data set with the 12,397 observations
+  #12 varaibles - the removal of the na does reduce the data set down.
+  #reverted to the initial data set.
+  #This provides the result of the 1932 Russian Famine with an estimated
+  #death toll of 5m people.
   
 
+  ##Q2 - Country with the highest number of people impacted by disaster.
+  
+  #Tested the data for those with na using the select function
+  ##3825 obs are then committed from the dataset.
 
+  disaster_totaleffected<- disasters_with_errors%>%
+    select(country_name, total_affected)%>%
+    filter(is.na == TRUE (total_affected))
+  
 
+  disaster_excludingthena <-disasters_with_errors%>%
+    select(country_name, total_affected)%>%
+    filter(is.na == FALSE (total_affected))
+  
 
-
-
+total_effected_dropped <- disasters_with_errors%>%
+  select(country_name, total_affected)%>%
+  drop_na(total_affected)%>%
+  arrange(desc(total_affected))
+total_effected_dropped
  
+##select function to identify country name and total affected.
+##drop the na value and then arranged in desc order to provide china as answer.
+
+total_china <-total_effected_dropped%>%
+  select(country_name, total_affected)%>%
+  filter(country_name  == "China")
 
 
-## Parsed with column specification:
-## cols(
-##   customerID = col_double(),
-##   monthly_charges = col_double(),
-##   total_charges = col_character(),
-##   payment_method = col_character(),
-##   churn = col_character()
-## )
+###
+total_china%>%
+  summarise(total_affected = Summary.numeric_version(total_affected))
+  mutate(sum/num(total_china))
+
+total_china
