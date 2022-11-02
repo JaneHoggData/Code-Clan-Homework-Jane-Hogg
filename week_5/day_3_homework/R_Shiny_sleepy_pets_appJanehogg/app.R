c1d406ec81b81c13a0faf43151e73e734d4d4b4b
JaneHoggData <112766934+JaneHoggData@users.noreply.github.com>
2022-11-02 19:31:32 +0000

    Pet Sleep App
    Plot not in there !!!

---
+++ week_5/day_3_homework/R_Shiny_sleepy_pets_appJanehogg/app.R
@@ -0,0 +1,68 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+
+##'Basic shiny app 
+
+library(shiny)
+library(tidyverse)
+library(bslib)
+
+library(readr)
+pets <- read_csv("pets.csv")
+View(pets)
+
+
+
+sleepy_pets <- sort(unique( pets$sleep))
+
+ui <- fluidPage(
+  titlePanel("Pets and Sleep"),
+  theme = bs_theme (bootswatch = "yeti"),
+  tags$i( "italics text can be places here"),
+  tags$br(),
+  tags$b( "for the bold text"),
+  tags$a("Pets at Home Website", href = "https://www.petsathome.com/" ),
+  tags$h1("This is an enourmous heading"),
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        inputId = "Gender",
+        label = "Male or Female ",
+        choices = c("Male", "Female")),
+      
+      
+      selectInput(
+        inputId = "sleep",
+        label = "How much?",
+        choices = sleep)
+        
+      ),
+    mainPanel(plotOutput("sleep_plot"))
+    
+  ))
+  
+
+
+server <- function(input, output) {
+  output$sleep_plot <- renderPlot(
+    sleepy_pets <- ggplot(pets) +
+      aes(x = sleep, fill = sex) +
+      scale_fill_manual(values=c('#E69F00', '#56B4E9')) +
+      geom_histogram(bins = 20, col = "white", show.legend = FALSE) + 
+      theme_light() + 
+      labs(y = "Gender", x = "Sleep/hrs") + 
+      facet_wrap(~ sex, nrow = 2)
+  )
+  
+}
+
+shinyApp(ui, server)
+
+
+
+
